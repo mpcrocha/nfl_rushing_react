28e196ff35459e6221da808d60f5904fe4dea316
marcos_rocha <marcos.rocha@m4u.com.br>
2019-11-26 08:16:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,188 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Table extends Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+      error: null,
+      isLoaded: false,
+      playerName: '',
+      rushings: [],
+      total: null,
+      per_page: null,
+      current_page: null,
+      current_page_url: null,
+      sort_params: []
+    };
+    
+   }
 
-export default App;
+   apiCall(url) {
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result.data);
+          console.log(result);
+          this.setState({
+            isLoaded: true,
+            rushings: result.data,
+            total: result.meta["total-pages"],
+            per_page: result.meta["per-page"],
+            current_page: result.meta["current-page"],
+            current_page_url: result.links.self
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  componentDidMount() {
+    this.apiCall("http://localhost:3001/api/v1/rushing_statistics")
+  }
+
+ renderTableHeader() {
+    return [<th key='0'>Player</th>, 
+    <th key='1'>Team</th>,  
+    <th key='2'>Pos</th>, 
+    <th key='3'>Att/G</th>, 
+    <th key='4'>Att</th>, 
+    <th key='5' onClick={(e)=>{this.sortData('yds',e)}}>Yrds</th>, 
+    <th key='6'>Avg</th>, 
+    <th key='7'>Yds/G</th>,
+    <th key='8' onClick={(e)=>{this.sortData('td',e)}}>TD</th>, 
+    <th key='9'onClick={(e)=>{this.sortData('lng',e)}}>Lng</th>, 
+    <th key='10'>1st</th>, 
+    <th key='11'>1st%</th>, 
+    <th key='12'>20+</th>, 
+    <th key='13'>40+</th>,
+    <th key='14'>FUM</th> ]
+ }
+
+ renderTableData() {
+    return this.state.rushings.map((rushing) => {
+       const { player, team, pos, 
+               att, 'att-g' : att_g, 
+               yds, avg, 
+               'yds-g': yds_g, 
+               td, lng, first, 
+               'first-pct' : first_pct, 
+               'twenty-plus' : twenty_plus , 
+               'forty-plus': forty_plus, 
+               fum } = rushing.attributes
+       return (
+          <tr key={player}>
+             <td>{player}</td>
+             <td>{team}</td>
+             <td>{pos}</td>
+             <td>{att}</td>
+             <td>{att_g}</td>
+             <td>{yds}</td>
+             <td>{avg}</td>
+             <td>{yds_g}</td>
+             <td>{td}</td>
+             <td>{lng}</td>
+             <td>{first}</td>
+             <td>{first_pct}</td>
+             <td>{twenty_plus}</td>
+             <td>{forty_plus}</td>
+             <td>{fum}</td>
+          </tr>
+       )
+    })
+ }
+
+ sortData(key,e) {
+  e.preventDefault();
+  var old_sort_params = this.state.sort_params.slice(0);
+  console.log("sort_params:" + old_sort_params);
+  if(this.state.sort_params.includes(key)) {
+    console.log("removing key: " + key);
+    var index = this.state.sort_params.indexOf(key);
+    this.state.sort_params.splice(index, 1);
+    this.state.sort_params.push('-'+key);
+  } else if(this.state.sort_params.includes('-'+key)){
+    var index = this.state.sort_params.indexOf('-'+key);
+    this.state.sort_params.splice(index, 1);
+    this.state.sort_params.push(key);
+  } else {
+    this.state.sort_params.push(key);
+  }
+
+  console.log("old_sort_params: " + old_sort_params);
+  console.log("sort_params:" + this.state.sort_params);
+  console.log(this.state.current_page_url);
+  var url = ''
+  if(old_sort_params.length == 0){
+    url = this.state.current_page_url + '&sort=' + this.state.sort_params;
+  } else {
+    console.log(old_sort_params.join(','))
+    url = this.state.current_page_url.replace('sort='+old_sort_params.join(','), 'sort='+ this.state.sort_params.join(','));
+  }
+  
+  console.log("url:" + url);
+  this.apiCall(url);
+ }
+
+ renderPageNumbers() {
+  const pageNumbers = [];
+  for (let i = 1; i <= this.state.total; i++) {
+      pageNumbers.push(i);
+  }
+
+  return pageNumbers.map(number => {
+    let classes = this.state.current_page === number ? 'active' : '';
+    if (number == 1 || number == this.state.total || (number >= this.state.current_page - 2 && number <= this.state.current_page + 2)) {
+      const url = this.state.current_page_url.replace('page%5Bnumber%5D='+this.state.current_page, 'page%5Bnumber%5D='+number)
+      return (
+        <span key={number} className={classes} onClick={() => this.apiCall(url)}>{number}</span>
+      );
+    }
+  })
+ }
+
+ render() { 
+    return (
+       <div>
+          <h1 id='title'>NFL Rushings</h1>
+          <div>
+            <label>Player Name</label>
+          
+            <input type="text" name={"search"} id={"search"}
+                 className="form-control"/>
+            <button onClick={(e)=>{this.filterPlayer(e)}}>Search Player</button>     
+          </div>
+          <table id='rushings'>
+             <tbody>
+                <tr>{this.renderTableHeader()}</tr>
+                {this.renderTableData()}
+             </tbody>
+          </table>
+
+          <div className='pagination'>
+            <span onClick={() => this.apiCall("http://localhost:3001/api/v1/rushing_statistics")}>&laquo;</span>
+            {this.renderPageNumbers()}
+          </div>
+
+       </div>
+    )
+ }
+ 
+ filterPlayer() {
+  const player = document.getElementById("search").value
+  console.log(player)
+  this.apiCall("http://localhost:3001/api/v1/rushing_statistics?player=" + player )
+ }
+} 
+
+export default Table
+
+
+
+
+
